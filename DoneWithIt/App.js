1cd1e6ce0016840994fcc59acd1c7bc1f32816b3
chundurisrikrishna <chundurisrikrishna@gmail.com>
2026-04-10 22:35:05 +0530

    Create App.js

---
+++ DoneWithIt/App.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+
+import CulturalPage from './screens/CulturalPage';
+import HomeScreen from './screens/HomeScreen';
+import LoginPage from './screens/LoginPage';
+import SplashScreen from './screens/SplashScreen';
+import SportsPage from './screens/SportsPage';
+import StudentForm from './screens/StudentForm';
+import TechPage from './screens/TechPage';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="StudentForm" component={StudentForm} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TechScreen" component={TechPage} />
+        <Stack.Screen name="CulturalScreen" component={CulturalPage} />
+        <Stack.Screen name="SportsScreen" component={SportsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
